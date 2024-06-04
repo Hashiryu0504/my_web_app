053b81d89c3cb5947db39ec8b867b6b0b02233de
Hashiryu0504 <166899544+Hashiryu0504@users.noreply.github.com>
2024-06-04 23:48:33 +0900

    fires commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def load_character_data(attribute):
         with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
+                print(row)
                 character_data.append(row)
     except UnicodeDecodeError:
         with open(csv_file_path, mode='r', newline='', encoding='shift_jis') as file:
